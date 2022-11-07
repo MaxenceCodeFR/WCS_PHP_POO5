9c7b12fad10b9de471af6f42ea52152aa8fa5175
Maxence Hattabi <105151136+MaxenceCodeFR@users.noreply.github.com>
2022-11-07 17:51:13 +0100

    Update Bicycle.php

--- quete_poo_abstract/Bicycle.php
+++ quete_poo_abstract/Bicycle.php
@@ -1,40 +1,21 @@
 <?php
 require_once 'Vehicle.php';
-require_once 'interfaces.php';
+require_once 'interface.php';
 
 class Bicycle extends Vehicle implements LightableInterface
 {
-    protected int $currentSpeed;
-
-
-
-    /**
-     * Get the value of currentSpeed
-     */
-    public function getCurrentSpeed()
-    {
-        return $this->currentSpeed;
-    }
-
-    /**
-     * Set the value of currentSpeed
-     *
-     * 
-     */
-    public function setCurrentSpeed($currentSpeed)
-    {
-        if ($currentSpeed >= 0) {
-            $this->currentSpeed = $currentSpeed;
-        }
-        return $this;
-    }
-
     public function switchOn(): bool
     {
-        if ($currentSpeed >= 10) {
+        if ($this->getCurrentSpeed() >= 10) {
             return $this->true;
         } else {
             return $this->false;
         }
+    }
+
+
+    public function switchOff()
+    {
+        return false;
     }
 }
